dbf8d2b125b100d9c20d9a9a6da6e943cf639813
Reck11 <118613899+Reck11@users.noreply.github.com>
2022-11-19 23:07:18 +0100

    much needed refactoring and cleanup

--- Assets/Scripts/Enemy/ZombieAI.cs
+++ Assets/Scripts/Enemy/ZombieAI.cs
@@ -10,26 +10,18 @@ public class ZombieAI : MonoBehaviour, IEnemy {
     public float Damage;
     public float Speed;
     [SerializeField]
-    private float _attackRange;
+    protected float _attackRange;
     [SerializeField]
-    private float _wanderRange;
+    protected float _detectionRange;
     [SerializeField]
-    private float _detectionRange;
-    [SerializeField]
-    private float _minWaitTime;
-    [SerializeField]
-    private float _maxWaitTime;
-    [SerializeField]
-    private float _maxHealthPoints;
+    protected float _maxHealthPoints;
     #endregion
     #region Fields
-    private Vector2 _wanderPoint;
-    private float _health;
-    private bool _shouldWander;
-    private GameObject _player;
-    private Pathfinding _pathfinding;
-    private State _state;
-    private float _distanceToPlayer;
+    protected float _health;
+    protected GameObject _player;
+    protected Pathfinding _pathfinding;
+    protected State _state;
+    protected float _distanceToPlayer;
 
     #endregion
 
@@ -38,19 +30,10 @@ public class ZombieAI : MonoBehaviour, IEnemy {
         _health = _maxHealthPoints;
         _player = GameObject.FindGameObjectWithTag(Tags.PLAYER);
         _pathfinding = GetComponent<Pathfinding>();
-        _shouldWander = true;
-    }
-
-    void Update() {
-        UpdateState();
-        if (_state == State.Idle)
-            Wander();
-        if (_state == State.Attack)
-            Attack();
     }
 
     // update state to determine what to do
-    private void UpdateState() {
+    protected void UpdateState() {
         if (_player == null)
             return;
 
@@ -61,45 +44,17 @@ public class ZombieAI : MonoBehaviour, IEnemy {
         else _state = State.Idle;
     }
 
-    // slowly pace around general area, randomly picking targets
-    private void Wander() {
-        _pathfinding.SetSpeed(Speed / 2);
-        if (!_pathfinding.hasTarget && _shouldWander) {
-            Vector2 randomDirection = Random.insideUnitCircle * _wanderRange;
-            randomDirection += (Vector2)transform.position;
-            NavMeshHit hit;
-            NavMesh.SamplePosition(randomDirection, out hit, _wanderRange, 1);
-            Vector2 finalPosition = hit.position;
-            if (Vector2.Distance(finalPosition, transform.position) < 1.0f)
-                return;
-            Move(finalPosition);
-        }
-        else if (_pathfinding.hasTarget && _pathfinding.GetDistance() < 0.5f) {
-            StartCoroutine(Waiter());
-        }
-    }
-
-    //wait between minWaitTime and maxWaitTime before moving again
-    private IEnumerator Waiter() {
-        if (!_shouldWander) // if moving is already inhibited, there is no need to start the couroutine
-            yield break;
-
-        _shouldWander = false;
-        yield return new WaitForSeconds(Random.Range(_minWaitTime, _maxWaitTime));
-        _shouldWander = true;
-    }
-
     // move by changing pathfinding script's target
-    private void Move(Vector2 target) {
+    protected void Move(Vector2 target) {
         _pathfinding.target = target;
     }
 
-    private void Attack() {
+    protected void Attack() {
         _pathfinding.SetSpeed(Speed);
         _pathfinding.target = _player.transform.position;
     }
     // hit player
-    private void Hit() {
+    protected void Hit() {
 
     }
     public void ReceiveDamage(float damage) {
@@ -107,7 +62,7 @@ public class ZombieAI : MonoBehaviour, IEnemy {
         if (_health <= 0)
             Die();
     }
-    private void Die() {
+    protected void Die() {
 
     }
 }
