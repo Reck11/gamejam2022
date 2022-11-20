6748c7a3de4369d6602fb7d0b4deaa0c17ae2c89
Reck11 <118613899+Reck11@users.noreply.github.com>
2022-11-20 06:21:15 +0100

    level colliders and pathfinding

---
+++ Assets/Scripts/Enviroment/Door.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+    [SerializeField] private GameObject leftDoor;
+    [SerializeField] private GameObject rightDoor;
+    [SerializeField] private float openAmount;
+
+    public void OpenDoor() {
+        leftDoor.transform.Translate(new Vector2 (leftDoor.transform.position.x - openAmount, leftDoor.transform.position.y));
+        rightDoor.transform.Translate(new Vector2(leftDoor.transform.position.x + openAmount, leftDoor.transform.position.y));
+    }
+}
